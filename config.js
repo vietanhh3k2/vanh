98f126d51c521f045c7e21f054d31c3c4cb4a1f6
vietanhh3k2 <118651352+vietanhh3k2@users.noreply.github.com>
2022-11-20 23:24:19 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
     titleWeb: "vananhcute",
     introTitle: 'Helu Vân Anhh',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
+    introDesc: `Tớ có vài điều muốn nói`,
+    btnIntro: '^^Ocee^^',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
     desc: 'Cho tớ theo đuổi Vân Anh nha ',
-    btnYes: 'Thích lắm <33',
+    btnYes: 'oke nhé <33',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 8 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Chờ tin nhắn của Vanh 😘',
+    question: 'Cậu nghĩ gì về tôy <3',
+    btnReply: 'Gửi ',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <333333333',
+    mess: 'Chờ tin nhắn của bạn 😘',
     messDesc: 'Noel mình qua đón công túa đi chơi nha 🥰.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://www.facebook.com/anhhss.nguyennviett.3/' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
